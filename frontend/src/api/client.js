8c3fb82075536ecee09348c5b19b53b034fa55c1
RaghuJ2411 <raghujpr9611@gmail.com>
2026-07-29 23:21:07 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: 'https://codemind-dna-api.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
